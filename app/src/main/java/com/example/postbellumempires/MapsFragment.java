661da50ec918038cb8fc40b735606d3ed7c83dad
Alexandre Monteiro <fc51023@alunos.fc.ul.pt>
2020-12-02 17:20:54 +0000

    Estive a tocar um pouco mais no mapa

--- app/src/main/java/com/example/postbellumempires/MapsFragment.java
+++ app/src/main/java/com/example/postbellumempires/MapsFragment.java
@@ -8,10 +8,16 @@ import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.daimajia.androidanimations.library.YoYo;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,24 +30,109 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MapsFragment extends Fragment implements OnMapReadyCallback{
+
+    private static final String TAG = "MapsFragment";
+
+    FloatingActionButton menu;
+    FloatingActionButton inventory;
+    FloatingActionButton armymenu;
+    FloatingActionButton profile;
+
+    private boolean clicked = false;
+
+    private Animation fromBottom;
+    private Animation toBottom;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        
         return inflater.inflate(R.layout.fragment_maps, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+        fromBottom = AnimationUtils.loadAnimation(this.getActivity(),R.anim.from_bottom_anim);
+        toBottom = AnimationUtils.loadAnimation(this.getActivity(),R.anim.to_bottom_anim);
+
+        menu = view.findViewById(R.id.menuButton);
+        inventory = view.findViewById(R.id.inventoryButton);
+        armymenu = view.findViewById(R.id.armyMenuButton);
+        profile = view.findViewById(R.id.profileButton);
+        
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAddButtonClicked();
+            }
+        });
+        inventory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG,"This is the inventory");
+            }
+        });
+        
+        armymenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG,"This is the army menu");
+            }
+        });
+        
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG,"This is your profile");
+            }
+        });
+        
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
+        }
+    }
+
+    private void onAddButtonClicked() {
+        setVisibility(clicked);
+        setAnimation(clicked);
+        clicked = !clicked;
+    }
+
+    private void setAnimation(Boolean clicked) {
+        if(!clicked){
+            profile.startAnimation(fromBottom);
+            armymenu.startAnimation(fromBottom);
+            inventory.startAnimation(fromBottom);
+        }else{
+            profile.startAnimation(toBottom);
+            armymenu.startAnimation(toBottom);
+            inventory.startAnimation(toBottom);
+        }
+    }
+
+    private void setVisibility(Boolean clicked) {
+        if(!clicked){
+            profile.setVisibility(View.VISIBLE);
+            profile.setClickable(true);
+            armymenu.setVisibility(View.VISIBLE);
+            armymenu.setClickable(true);
+            inventory.setVisibility(View.VISIBLE);
+            inventory.setClickable(true);
+        }else{
+            profile.setVisibility(View.INVISIBLE);
+            profile.setClickable(false);
+            armymenu.setVisibility(View.INVISIBLE);
+            armymenu.setClickable(false);
+            inventory.setVisibility(View.INVISIBLE);
+            inventory.setClickable(false);
         }
     }
 

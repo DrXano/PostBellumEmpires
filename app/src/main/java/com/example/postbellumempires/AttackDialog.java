5947233f2c54f07e458403f1938328874e1fe080
Alexandre Monteiro <fc51023@alunos.fc.ul.pt>
2020-12-03 03:57:57 +0000

    GUI feito

---
+++ app/src/main/java/com/example/postbellumempires/AttackDialog.java
@@ -0,0 +1,44 @@
+package com.example.postbellumempires;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+
+public class AttackDialog extends Dialog implements View.OnClickListener{
+
+    public ImageButton cancel;
+    public Button attack;
+
+    public AttackDialog(@NonNull Context context) {
+        super(context, android.R.style.Theme_Black_NoTitleBar);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.attack_dialog);
+        attack = findViewById(R.id.attackButton2);
+        cancel = findViewById(R.id.cancelButton2);
+        attack.setOnClickListener(this);
+        cancel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.attackButton2:
+                break;
+            case R.id.cancelButton2:
+                dismiss();
+                break;
+            default:
+                break;
+        }
+        dismiss();
+    }
+}
--- app/src/main/java/com/example/postbellumempires/MapsFragment.java
+++ app/src/main/java/com/example/postbellumempires/MapsFragment.java
@@ -11,26 +11,19 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import com.daimajia.androidanimations.library.YoYo;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -208,12 +201,22 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback{
 
             @Override
             public boolean onMarkerClick(Marker marker) {
+
+                Dialog d = new PostDialog(getActivity());
+                d.getWindow().setBackgroundDrawable(new ColorDrawable(Color.argb(230, 0, 0, 0)));
+                d.show();
+
+                /*
                 Dialog d = new Dialog(getActivity(), android.R.style.Theme_Black_NoTitleBar);
-                d.getWindow().setBackgroundDrawable(new ColorDrawable(Color.argb(100, 0, 0, 0)));
-                d.setContentView(R.layout.place_menu);
+                d.getWindow().setBackgroundDrawable(new ColorDrawable(Color.argb(230, 0, 0, 0)));
+                d.setContentView(R.layout.post_dialog);
                 d.setCancelable(true);
                 d.show();
+                */
+
+
                 return true;
+
             }
         });
     }

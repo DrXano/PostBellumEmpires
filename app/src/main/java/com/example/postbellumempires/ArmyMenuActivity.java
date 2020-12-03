021d5bd13e254d17ff61dea1ff0855bdc918850c
Alexandre Monteiro <fc51023@alunos.fc.ul.pt>
2020-12-03 01:30:39 +0000

    So falta um pequeno pormenor

--- app/src/main/java/com/example/postbellumempires/ArmyMenuActivity.java
+++ app/src/main/java/com/example/postbellumempires/ArmyMenuActivity.java
@@ -1,11 +1,25 @@
 package com.example.postbellumempires;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class ArmyMenuActivity extends AppCompatActivity {
+
+    Fragment train = new ArmyTrainingFragment();
+    Fragment upgrade = new ArmyUpgradeFragment();
+
+    private TabAdapter adapter;
+    private ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +31,51 @@ public class ArmyMenuActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Army");
+
+        adapter = new TabAdapter(getSupportFragmentManager());
+        pager = findViewById(R.id.viewPager);
+        pager.setAdapter(adapter);
+
+        TabLayout lay = findViewById(R.id.tabLayout);
+        lay.setupWithViewPager(pager);
+    }
+
+    public class TabAdapter extends FragmentPagerAdapter {
+
+        public TabAdapter(@NonNull FragmentManager fm) {
+            super(fm, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = null;
+            switch (position) {
+                case 0:
+                    fragment = train;
+                    break;
+                case 1:
+                    fragment = upgrade;
+                    break;
+            }
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Train";
+                case 1:
+                    return "Upgrade";
+            }
+            return null;
+        }
     }
 }
